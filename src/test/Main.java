4dde0f9b0c79c918ac989ca07579b072a28874f4
Omperkash Maheshwari <omperkash.maheshwari1981@gmail.com>
2018-03-06 16:39:43 +0100

    first commit

---
+++ src/test/Main.java
@@ -0,0 +1,129 @@
+
+package test;
+
+
+
+public class Main {
+
+	private static int num;
+    private static char ch;
+    private static boolean b;
+    private String str;
+	
+	public static void main(String[] args) {
+
+
+		Main m= new Main();
+		
+			
+	}
+		
+	  Main()
+		{
+			
+			System.out.println(num);
+			System.out.println(ch);
+			System.out.println(b);
+			System.out.println(str);
+			
+		}
+	
+	
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		String name="Omperkash Maheshwari";
+//		String firstname=name.substring(0, name.indexOf(" "));
+//		
+//		String lastname=name.substring(name.indexOf(" ")+1,name.length());
+//		System.out.println(firstname.toUpperCase());
+//		System.out.println(lastname.toUpperCase());
+		
+		
+		
+		
+		
+//		char[] name= {'O','M','P','E','R','K','A','S','H'};
+//		
+//		System.out.println(name);
+		
+		
+		
+//		int num=50;
+//		for (int i=0; i<num;++i)
+//		{
+//			System.out.print(" "+i*2);
+//		}
+		
+		
+		
+		
+		
+		
+		
+//		 java.util.Date now = new java.util.Date();
+//		 java.util.Date now2 = new java.util.Date();
+//		 
+// 
+// String str= new String("Hello!");
+// String str2=str+" om";
+// str=null;
+// System.out.println(str2);
+// 
+// String nowString= now.toString();
+// System.out.println(now);
+// System.out.println(nowString);
+//
+//now2=now;	
+//now=null;
+//
+// System.out.println(now);
+//System.out.println(now2);
+		
+		
+		
+		
+		
+		
+		//		String namn="Ulf";
+//		System.out.println(namn.toUpperCase());
+//		System.out.println(namn.length());
+//		
+//		String num="555";
+//		int number=Integer.parseInt(num);
+//		
+//		System.out.println(number);
+		
+		
+//		System.out.println(1/3);
+//		System.out.println(1.0/3);
+//		System.out.println(1/3*45.78);
+//	
+//		System.out.println(45/34.0);
+//		System.out.println(45/34);
+//		
+		
+		
+		
+		
+		
+	
+
+
